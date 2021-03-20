9dfcff6a02141b9a400fd0d1847a7e79c9da6e98
SACHINER <Sachine.Reddy@vubiquity.co.uk>
2021-03-20 12:20:15 +0200

    changes for assignment4

---
+++ api/scripts/init.mongo.js
@@ -0,0 +1,25 @@
+/* eslint linebreak-style: ["error", "windows"] */
+/* global db print */
+/* eslint no-restricted-globals: "off" */
+
+db.products.remove({});
+
+const productsDB = [
+  {
+    id: 1,
+    name: 'Blue Shirt',
+    price: 16.99,
+    category: 'Shirts',
+    image: 'https://www.istockphoto.com/photo/smiling-young-man-pointing-down-gm996927722-269751810',
+  },
+];
+
+db.products.insertMany(productsDB);
+const count = db.products.count();
+print('Inserted', count, 'products');
+
+db.counters.remove({ _id: 'products' });
+db.counters.insert({ _id: 'products', current: count });
+
+db.products.createIndex({ id: 1 }, { unique: true });
+db.products.createIndex({ name: 1 });
